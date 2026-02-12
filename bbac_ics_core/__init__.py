dcbced7429c9fdaa179fceed00fc52731dcb0e22
Alexandre do Nascimento Silva <121621080+a-nsilva@users.noreply.github.com>
2026-02-12 08:39:17 -0300

    Create __init__.py

---
+++ bbac_ics_core/__init__.py
@@ -0,0 +1,12 @@
+"""
+BBAC ICS Framework
+Adaptive Behavior-Based Access Control for Industrial Control Systems
+"""
+
+__version__ = '1.0.0'
+__author__ = 'BBAC Research Team'
+
+from .utils.config_loader import ConfigLoader
+from .utils.data_structures import *
+
+__all__ = ['ConfigLoader']
